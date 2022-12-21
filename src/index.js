fe2b3390509ba8546cc9ca29557570ca06e30e41
boris1592 <buzin.boria@gmail.com>
2022-12-21 10:28:38 +0300

    saving chats to file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 const { Telegraf } = require('telegraf');
-const { config } = require('dotenv');
 const { CronJob } = require('cron');
+const { config } = require('dotenv');
+const { readFileSync, writeFileSync, existsSync } = require('fs');
+
+if (!existsSync('data.json')) {
+  writeFileSync('data.json', '{}');
+}
 
 config();
+const chatMessages = JSON.parse(readFileSync('data.json').toString());
 
-const bot = new Telegraf(process.env.TOKEN);
-const chatMessages = {};
+function saveData() {
+  writeFileSync('data.json', JSON.stringify(chatMessages));
+}
 
 function addLeadingZeros(number, totalLength) {
   return String(number).padStart(totalLength, '0');
 }
 
+const bot = new Telegraf(process.env.TOKEN);
+
 bot.command('start_countdown', async (ctx) => {
   chatMessages[ctx.chat.id] = (
     await ctx.reply('В ближайшее время тут появится таймер')
   ).message_id;
+  saveData();
 });
 
 bot.command('stop_countdown', (ctx) => {
   if (ctx.chat.id in chatMessages) delete chatMessages[ctx.chat.id];
+  saveData();
 });
 
 bot.launch();
@@ -34,10 +45,21 @@ const job = new CronJob('* * * * *', () => {
   const hours = addLeadingZeros(Math.floor((remainingTime / 60) % 24), 2);
   const minutes = addLeadingZeros(remainingTime % 60, 2);
   const message = `До нового года осталось: ${days}:${hours}:${minutes}`;
+  const toRemove = [];
 
   for (const chatId in chatMessages) {
-    bot.telegram.editMessageText(chatId, chatMessages[chatId], null, message);
+    try {
+      bot.telegram.editMessageText(chatId, chatMessages[chatId], null, message);
+    } catch (e) {
+      toRemove.push(chatId);
+    }
   }
+
+  for (const chatId of toRemove) {
+    delete chatMessages[chatId];
+  }
+
+  saveData();
 });
 
 job.start();
